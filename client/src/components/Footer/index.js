a9151a1e9e4ecc3d12fa6f30c232b4acadc5f604
Kaishan <kaishanpatel26@gmail.com>
2023-01-31 21:05:39 +0000

    CW - Implement Footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <p style={{ color: 'white' }}>Kaishan Patel - CatWiki For BlowFish Studios</p>
+    )
+}
+
+export default Footer;
